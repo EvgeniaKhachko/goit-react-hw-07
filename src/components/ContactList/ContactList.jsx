46137196def2014a99b88ddb3b22111c423b4e19
kapitu <ketkap007@gmail.com>
2024-12-21 14:33:29 +0200

    fix-ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { selectContacts } from "../../redux/contactsSlice";
-import { selectFilter } from "../../redux/filtersSlice";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 import s from "./ContactList.module.css";
 import Contact from "../Contact/Contact";
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const newName = useSelector(selectFilter);
-  const filteredContacts = contacts.filter((contact) => {
-    return contact.name.toLowerCase().includes(newName);
-  });
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <>
